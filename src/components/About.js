a9a71d0cac987ffed453489bc7d955774a8c3bd5
Prabin Dangol <dprabin23@gmail.com>
2025-06-16 22:40:21 +0545

    about error
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/components/About.js
+++ src/components/About.js
@@ -19,13 +19,12 @@ export default function AboutSection() {
           <h2 className="text-4xl font-bold mb-4 text-gray-800 dark:text-white">
             About Me
           </h2>
-          <p className="text-lg text-gray-600 dark:text-gray-300 mb-4 leading-relaxed">
-  {`Hello! I'm a passionate `}
+  <p className="text-lg text-gray-600 dark:text-gray-300 mb-4 leading-relaxed">Hello! I&apos;m a passionate 
   <span className="text-amber-500 font-semibold">Web Developer</span>
   {` and `}
   <span className="text-amber-500 font-semibold">Graphic Designer</span>
-  {` with a passion for building creative, user-friendly digital experiences.`}
-</p>
+  {` with a passion for building creative, user-friendly digital experiences.`}</p>
+
           <p className="text-gray-600 dark:text-gray-400">
             With a strong foundation in front-end development and visual
             storytelling, I bridge the gap between functionality and aesthetics.
